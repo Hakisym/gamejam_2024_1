65770df9df8de4a2d33ac88424783c3621298a29
HenryFXG <1480201936@qq.com>
2024-02-16 21:34:02 +0800

    AIr hockey prototype

--- Assets/Game/Scripts/PaddleController.cs
+++ Assets/Game/Scripts/PaddleController.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class PaddleController : MonoBehaviour
 {
-    public float force;
+    public float force = 10f;
+    public float moveSpeed = 10f;
+    public float smoothTime = 0.3f;
+    public float coefficientOfRestitution = 0.8f;
+
     private Vector2 currVel;
     private Vector2 prevPos;
-    private float speed;
+    private Vector2 smoothVelocity;
+    private float magnitude;
+
 
     void Update()
     {
@@ -15,40 +21,38 @@ public class PaddleController : MonoBehaviour
         CalcVelocity();
     }
 
-    private void CalcVelocity()
-    {
-        currVel = ((Vector2)transform.position - prevPos) / Time.deltaTime;
-        speed = currVel.magnitude;
-        speed = Mathf.Clamp(speed, 0, 2);
-
-        prevPos = transform.position;
-    }
-
     private void MouseFollow()
     {
         var pos = Input.mousePosition;
         pos.z = 10f;
-        transform.position = Camera.main.ScreenToWorldPoint(pos);
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(pos);
+        transform.position = Vector2.SmoothDamp(transform.position, mousePos, ref smoothVelocity, smoothTime, moveSpeed);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void CalcVelocity()
     {
-        if (collision.CompareTag("Hitable"))
-        {
-            var dir = collision.ClosestPoint(collision.transform.position) - (Vector2)transform.position;
-            collision.GetComponent<Rigidbody2D>().AddForce(dir.normalized * force * speed, ForceMode2D.Impulse);
-        }
+        currVel = ((Vector2)transform.position - prevPos) / Time.deltaTime;
+        magnitude = currVel.magnitude;
+        magnitude = Mathf.Lerp(0, 1, magnitude/moveSpeed);
+
+        prevPos = transform.position;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.transform.CompareTag("Hitable"))
         {
+            //bounce
             Rigidbody2D collidingRB = collision.transform.GetComponent<Rigidbody2D>();
-            collidingRB.velocity = Vector3.Reflect(collision.transform.GetComponent<PuckHandler>().velocity, -collision.contacts[0].normal);
+            Vector2 newVelocity = Vector3.Reflect(collision.transform.GetComponent<PuckHandler>().velocity, -collision.contacts[0].normal);
 
+            // Apply coefficient of restitution
+            newVelocity *= coefficientOfRestitution;
+            collidingRB.velocity = newVelocity;
+
+            //hit the ball
             var dir = collision.contacts[0].point - (Vector2)transform.position;
-            collision.transform.GetComponent<Rigidbody2D>().AddForce(dir.normalized * force * speed, ForceMode2D.Impulse);
+            collision.transform.GetComponent<Rigidbody2D>().AddForce(dir.normalized * force * magnitude, ForceMode2D.Impulse);
         }
     }
 }
--- Assets/Game/Scripts/ScreenCollider.cs
+++ Assets/Game/Scripts/ScreenCollider.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class ScreenCollider : MonoBehaviour
 {
+    public float coefficientOfRestitution = 0.8f;
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.transform.CompareTag("Hitable"))
         {
+            //bounce
             Rigidbody2D collidingRB = collision.transform.GetComponent<Rigidbody2D>();
-            collidingRB.velocity = Vector3.Reflect(collision.transform.GetComponent<PuckHandler>().velocity, -collision.contacts[0].normal);
+            Vector2 newVelocity = Vector3.Reflect(collision.transform.GetComponent<PuckHandler>().velocity, -collision.contacts[0].normal);
+
+            // Apply coefficient of restitution
+            newVelocity *= coefficientOfRestitution;
+            collidingRB.velocity = newVelocity;
         }
     }
 }
